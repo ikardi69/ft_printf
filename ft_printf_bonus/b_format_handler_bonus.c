9040de1185ee802627241f5c7cd023b4e988ed5b
Mouhssine <169074517+ikardi69@users.noreply.github.com>
2024-11-27 22:57:38 +0100

    ft_printf

--- ft_printf_bonus/b_format_handler_bonus.c
+++ ft_printf_bonus/b_format_handler_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mteffahi <mteffahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 02:43:51 by mteffahi          #+#    #+#             */
-/*   Updated: 2024/11/27 17:59:01 by mteffahi         ###   ########.fr       */
+/*   Updated: 2024/11/27 21:48:00 by mteffahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	ft_bonus_format_handler(va_list args, const char *s)
 	size = 0;
 	if (s[size] == '.')
 	{
-		s++;
-		size += ft_dot(args, s);
+		//s++;
+		size += ft_dot(args, s+1);
 	}
 	return (size);
 }
