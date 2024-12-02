29f123df90d5a9a869da336d55f01806165a5a4e
Mouhssine <169074517+ikardi69@users.noreply.github.com>
2024-12-02 17:53:52 +0100

    ft_printf field minimum

--- ft_printf_bonus/ft_field_minimum_width_bonus.c
+++ ft_printf_bonus/ft_field_minimum_width_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mteffahi <mteffahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 14:29:45 by mteffahi          #+#    #+#             */
-/*   Updated: 2024/12/02 15:38:16 by mteffahi         ###   ########.fr       */
+/*   Updated: 2024/12/02 16:42:46 by mteffahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,15 @@ int	ft_field_minimum_width(va_list args, const char *s)
 	int	width;
 	int	size;
 	int	i;
+	int	m;
 
 	width = ft_atoi(s);
 	i = ft_skip(s);
 	size = 0;
-	if (s[i] == 's')
-		
-	size += padding(width);
+	if (s[i] == 'c')
+		m += 1;
+	else if (s[i] == 's')
+	size += padding(width, m);
 	size += format_handler(args, (const char)s+i);
 	return (size);
 }
